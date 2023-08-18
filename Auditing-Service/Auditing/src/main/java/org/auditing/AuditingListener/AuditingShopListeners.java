aaa979fab52119c1e44eb00c1e08bef5d96de74f
VinyS2 <viniviusmarcus00@gmail.com>
2023-08-17 23:12:40 -0300

    create listeners shop

---
+++ Auditing-Service/Auditing/src/main/java/org/auditing/AuditingListener/AuditingShopListeners.java
@@ -0,0 +1,33 @@
+package org.auditing.AuditingListener;
+
+import org.auditing.AuditingCreatedEvent;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import static org.auditing.Config.RabbitMQShop.*;
+
+@Component
+public class AuditingShopListeners {
+
+    @RabbitListener(queues = SHOP_QUEUE_NAME_CREATED)
+    public void onShopCreated(AuditingCreatedEvent event){
+        System.out.println("QUEUE CREATED: ");
+        System.out.println("Id recebido " + event.getId());
+        System.out.println("Name recebido " + event.getName());
+        System.out.println("Valor recebido " + event.getValue());
+    }
+    @RabbitListener(queues = SHOP_QUEUE_NAME_UPDATE)
+    public void onShopUpdate(AuditingCreatedEvent event){
+        System.out.println("QUEUE UPDATED: ");
+        System.out.println("Id recebido " + event.getId());
+        System.out.println("Name recebido " + event.getName());
+        System.out.println("Valor recebido " + event.getValue());
+    }
+    @RabbitListener(queues = SHOP_QUEUE_NAME_DELETE)
+    public void onShopDelete(AuditingCreatedEvent event){
+        System.out.println("QUEUE DELETED: ");
+        System.out.println("Id recebido " + event.getId());
+        System.out.println("Name recebido " + event.getName());
+        System.out.println("Valor recebido " + event.getValue());
+    }
+}
