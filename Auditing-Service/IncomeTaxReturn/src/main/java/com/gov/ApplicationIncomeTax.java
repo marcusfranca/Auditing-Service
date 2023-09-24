74ee3923523240e106195b5f2d2e8cbe71faff6e
VinyS2 <viniviusmarcus00@gmail.com>
2023-09-24 00:27:31 -0300

    alteracoes do gitgnore e inicializacao do novo servico.

---
+++ Auditing-Service/IncomeTaxReturn/src/main/java/com/gov/ApplicationIncomeTax.java
@@ -0,0 +1,11 @@
+package com.gov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationIncomeTax {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationIncomeTax.class, args);
+    }
+}
